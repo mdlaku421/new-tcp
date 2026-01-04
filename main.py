02ef810544ccf7f45f3b9d418d4e5c21e8178146
mdlaku421 <mahiraislam292@gmail.com>
2026-01-04 19:29:37 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1619,8 +1619,8 @@ async def GeNeRaTeAccEss(uid , password):
         "Accept-Encoding": "gzip, deflate, br",
         "Connection": "close"}
     data = {
-        "uid": uid,
-        "password": password,
+        "uid": " '4373451022",
+        "password": "4EF2CD36A50A2378755FFEDDFF7B70A8A05D4FBFA613AEA90010F30981C69034",
         "response_type": "token",
         "client_type": "2",
         "client_secret": "2ee44819e9b4598845141067b281621874d0d5d7af9d8f7e00c1e54715b7d1e3",
